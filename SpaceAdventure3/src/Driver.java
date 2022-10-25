14a1a0f29b1b8a3e512167bd91b270c1c80efcf1
lordphinix <116685847+lordphinix@users.noreply.github.com>
2022-10-25 19:13:00 -0300

    Reupload
    Uploading again today since original featured a test that wasn't supposed to be public

--- SpaceAdventure3/src/Driver.java
+++ SpaceAdventure3/src/Driver.java
@@ -4,13 +4,13 @@ import java.util.*;//remove later
 //Main Class that creates a Title Object and runs it's sole method.
 public class Driver {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in); // rm later
-        Info testInfo =new Info("test player", "test ship");
-        Tools testTools = new Tools();
-        Combat testCombat = new Combat(sc, testInfo, testTools);
-        testCombat.play(sc);
+        //Scanner sc = new Scanner(System.in); // rm later
+        //Info testInfo =new Info("test player", "test ship");
+        //Tools testTools = new Tools();
+        //Combat testCombat = new Combat(sc, testInfo, testTools);
+        //testCombat.play(sc);
         //end of rm
-        //Title title = new Title();
-        //Drivertitle.show();
+        Title title = new Title();
+        title.show();
     }
 }
